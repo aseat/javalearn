2de1280aab172e3d29db2bbf2b7dc250fa677246
aseat <lemnlsdrye231mk@gmail.com>
2021-10-27 16:15:29 +0900

    java silver

--- Character.java
+++
@@ -1,8 +0,0 @@
-public class Character {
-   public static void character(){
-    Sample s = new Sample();
-    String val = s.setValue("hello");
-  s.getValue();
-  System.out.println(val);
-  }
-} 
