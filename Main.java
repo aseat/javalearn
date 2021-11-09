6fb22fa4c1f3913d03c95d926fe0a07f4a66d959
aseat <lemnlsdrye231mk@gmail.com>
2021-11-09 10:52:22 +0900

    if文
    {}なしの場合一番下は結果関係なく実行する

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    int a= 10,b=10,c=10,d=10; 
-    System.out.println(a++);
-     System.out.println(++b);
-     System.out.println(c--);
-     System.out.println(--d);
-     a= 10;b=10;c=10;d=10;
-     b=++a;
-     System.out.println("a="+a+"b="+b);
-     d=c++;
-     System.out.println("c="+c+"d="+d);
+    int num=10;
+    if(num<=10)
+    System.out.println("x");
+    System.out.println("xm");
   }
 } 
