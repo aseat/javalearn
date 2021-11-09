3949be2ab31066c77340201e097533c4bd0dcf10
aseat <lemnlsdrye231mk@gmail.com>
2021-11-09 10:01:59 +0900

    javasilver

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
   public static void main(String[] args) {
-    main(args); 
+    int a= 10,b=10,c=10,d=10; 
+    System.out.println(a++);
+     System.out.println(++b);
+     System.out.println(c--);
+     System.out.println(--d);
+     a= 10;b=10;c=10;d=10;
+     b=++a;
+     System.out.println("a="+a+"b="+b);
+     d=c++;
+     System.out.println("c="+c+"d="+d);
   }
 } 
