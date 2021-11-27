d22f6d4a0e1baddaa26d200cb38072d7afefc901
aseat <lemnlsdrye231mk@gmail.com>
2021-11-27 18:14:39 +0900

    hello world

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
-public static void Main(String args[]){
-  disp(10, 7, 8);
-}
-
-private static void disp(int... num){
-  for (int i = 0; i < num.length; i++){
-    System.out.println(num[i]);
-  }
-}
- 
+class Main {
+    public static void main(String[] args) {
+    System.out.println("hello world");
+    }
+} 
